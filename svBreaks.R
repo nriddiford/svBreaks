2c443d01d31afa0d2add337ed6d778dda4d34a56
nriddiford <nickriddiford@me.com>
2017-09-14 16:01:58 +0200

    rainfall

--- svBreaks.R
+++ svBreaks.R
@@ -4,12 +4,12 @@ library(dplyr)
 library(RColorBrewer)
 
 
-getData <- function(infile = "data/all_bps_new.txt"){
+getData <- function(infile = "data/all_bps.txt"){
   bp_data<-read.delim(infile, header = F)
   colnames(bp_data) <- c("event", "bp_no", "sample", "chrom", "bp", "gene", "feature", "type", "length")
   
   #filter on chroms
-  bp_data<-filter(bp_data, chrom != "Y" & chrom != "4")
+  #bp_data<-filter(bp_data, chrom != "Y" & chrom != "4")
   #filter out samples
   bp_data<-filter(bp_data, sample != "A373R1" & sample != "A373R7" & sample != "A512R17" )
   bp_data<-droplevels(bp_data)
@@ -621,31 +621,32 @@ bpRainfall <- function(){
   bp_data<-getData()
   
   bp_data<-filter(bp_data, sample != "A373R11" & sample != 'A373R13')
-  distances<-do.call(rbind, lapply(split(bp_data[order(bp_data$chrom, bp_data$pos),], bp_data$chrom[order(bp_data$chrom, bp_data$pos)]),
+  distances<-do.call(rbind, lapply(split(bp_data[order(bp_data$chrom, bp_data$bp),], bp_data$chrom[order(bp_data$chrom, bp_data$bp)]),
                                    function(a) 
                                      data.frame(a,
-                                                dist=c(diff(a$pos), NA),
-                                                logdist = c(log10(diff(a$pos)), NA))
+                                                dist=c(diff(a$bp), NA),
+                                                logdist = c(log10(diff(a$bp)), NA))
   )
   )
   
   
   distances$logdist[is.infinite(distances$logdist)] <- 0
-  distances<-filter(distances, chrom != 4)
+  distances<-filter(distances, chrom != 4, chrom != "Y")
   
   p<-ggplot(distances)
-  p<-p + geom_point(aes(pos/1000000, logdist, colour = grouped_trans))
+  p<-p + geom_point(aes(bp/1000000, logdist, colour = sample))
   p <- p + cleanTheme() +
     theme(axis.text.x = element_text(angle=45, hjust = 1),
           panel.grid.major.y = element_line(color="grey80", size = 0.5, linetype = "dotted")
     )
   
-  p<-p + facet_wrap(~chrom, scale = "free_x", ncol = 2)
-  p<-p + scale_x_continuous("Mbs", breaks = seq(0,33,by=1), limits = c(0, 33), expand = c(0.01, 0.01))
+  p<-p + facet_wrap(~chrom, scale = "free_x", ncol = 5)
+  #p<-p + scale_x_continuous("Mbs", breaks = seq(0,33,by=1), limits = c(0, 33), expand = c(0.01, 0.01))
+  p<-p + scale_x_continuous("Mbs", breaks = seq(0,max(distances$bp),by=10))
   
   rainfall_out<-paste("rainfall.pdf")
   cat("Writing file", rainfall_out, "\n")
-  ggsave(paste("plots/", rainfall_out, sep=""), width = 20, height = 10)
+  ggsave(paste("plots/", rainfall_out, sep=""), width = 15, height = 5)
   
   p
 }

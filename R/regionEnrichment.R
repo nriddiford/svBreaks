29f20826d93053a9ca84748f9eeb107ea98c2513
nriddiford <nickriddiford@me.com>
2018-04-25 13:50:23 +0200

    Implement bedtools merge and intersect to remove unmappable regions from files. Recalculate length for slop

--- R/regionEnrichment.R
+++ R/regionEnrichment.R
@@ -2,17 +2,16 @@
 #'
 #' Calculate the enrichment of SVs in genomic regions
 #' @keywords enrichment
-#' @import dplyr
+#' @import tidyverse
 #' @import data.table
 #' @export
 
-bpRegionEnrichment <- function(bedDir='inst/extdata/bed', breakpoints=NA, slop=0, plot=TRUE, genome_length=118274340 ){
-  require(data.table) ## 1.9.3
+bpRegionEnrichment <- function(bedDir='inst/extdata/bed/mappable', breakpoints=NA, slop=0, plot=TRUE, genome_length=118274340, intersect=FALSE, write=FALSE ){
   if(is.na(breakpoints)){
     breakpoints <- getData(genotype=='somatic_tumour', !sample %in% c("A373R1", "A373R7", "A512R17", "A373R11", "A785-A788R1", "A785-A788R11", "A785-A788R3", "A785-A788R5", "A785-A788R7", "A785-A788R9"))
     bps <- breakpoints %>% 
       dplyr::rename(start = bp) %>% 
-      mutate(end = start+1) %>%
+      dplyr::mutate(end = start+1) %>%
       select(chrom, start, end)
     
   } else{
@@ -33,19 +32,41 @@ bpRegionEnrichment <- function(bedDir='inst/extdata/bed', breakpoints=NA, slop=0
   # cat("Analysing all files in directory:", bedFiles, "\n")
   for (f in fileNames){
     # cat("Analysing file:", f, "\n")
+    
     regions <- read.delim(paste(bedDir, f, sep='/'), header = F)
     regions <- regions[,c(1,2,3)]
     colnames(regions) <- c("chrom", "start", "end")
     
     myChroms <- c("2L", "2R", "3L", "3R", "X", "Y", "4")
-
+    
     # This slop wont handle cases where the adjusted regions overlap (they will be counted twice...)
     regions <- regions %>% 
       filter(chrom %in% myChroms) %>% 
       mutate(start = start - slop) %>% 
       mutate(end = end + slop) %>% 
+      arrange(chrom) %>% 
       droplevels()
-  
+    
+    if(intersect){
+      cat("Analysing file:", f, "\n")
+      merged_regions <- mergeOverlaps(regions, dataframe = T)
+      cat("Merged file:", f, "\n")
+      
+      intersected_regions <- subtractUnmappable(merged_regions, dataframe=T)
+      cat("Intersected file:", f, "\n")
+      intersected_regions$chr <- as.factor(intersected_regions$chr)
+      regions <- intersected_regions %>% 
+        dplyr::rename(chrom=chr)
+      
+      if(write){
+        cat("Writing bed file:", f, "\n")
+        basename <- tools::file_path_sans_ext(f)
+        basename <- paste(basename, '_merged_intersected_', slop, '.bed', sep='')
+        writeBed(df=regions, name = basename)
+      }
+
+    }
+   
     # setkey = d1 for breakpoints, d2 for regions
     r <- data.table(regions)
     b <- data.table(bps)
@@ -150,12 +171,11 @@ bpRegionEnrichment <- function(bedDir='inst/extdata/bed', breakpoints=NA, slop=0
 #'
 #' Plot the enrichment of SVs in genomic features
 #' @keywords enrichment
-#' @import tidyverse
+#' @import dplyr
 #' @import plotly
 #' @export
 
 Volcano <- function(df){
-  require(plotly)
   feature_enrichment <- df
 
   # feature_enrichment <- transform(feature_enrichment, feature = reorder(feature, -Log2FC))
@@ -174,6 +194,7 @@ Volcano <- function(df){
           mode = 'markers',
           # height = 1200,
           # width = 1000,
+          # frame = ~p_val,
           text = ~paste("Feature: ", feature, "\n",
                         "Observed: ", observed, "\n",
                         "Expected: ", expected, "\n",
@@ -184,6 +205,84 @@ Volcano <- function(df){
            yaxis = list(title="-Log10(p)", titlefont = ax),
            showlegend = FALSE)
 }
+
+
+# mergeOverlaps
+#'
+#' Implements bedtools merge to merge overlapping regions in bedfile
+#' @keywords bed merge
+#' @import bedr
+#' @export
+#' 
+mergeOverlaps <- function(f, dataframe=FALSE){
+  
+  if(!dataframe){
+    bedFile <- read.delim(f, header = F)
+  } else{
+    bedFile <- f
+  }
+  bedFile <- bedFile[,c(1,2,3)]
+  colnames(bedFile) <- c("chr", "start", "end")
+  bedFile$chr <- as.character(bedFile$chr)
+  bedFile.sort <- bedr.sort.region(bedFile, check.chr = FALSE)
+  bedFile.merged <- bedr(engine = "bedtools", input = list(i = bedFile.sort), method = "merge", params = "",  check.chr = FALSE)
+  
+  return(data.frame(bedFile.merged, row.names = NULL))
+}
+
+
+# subtractUnmappable
+#'
+#' Implements bedtools subtract to remove unmappable regions in bedfiles
+#' @keywords bed subtract
+#' @import bedr
+#' @export
+
+subtractUnmappable <- function(f, u='~/Documents/Curie/Data/Genomes/Dmel_v6.12/Mappability/dmel6_unmappable_75.bed', dataframe=FALSE){
+  if(!dataframe){
+    bedFile <- read.delim(f, header = F)
+  } else{
+    bedFile <- f
+  }
+  bedFile <- bedFile[,c(1,2,3)]
+  colnames(bedFile) <- c("chr", "start", "end")
+  bedFile$chr <- as.character(bedFile$chr)
+  bedFile.sort <- bedr.sort.region(bedFile, check.chr = FALSE, method = 'lexicographical')
+  
+  excludeFile <- read.delim(u, header = F)
+  excludeFile <- excludeFile[,c(1,2,3)]
+  colnames(excludeFile) <- c("chr", "start", "end")
+  excludeFile$chr <- as.character(excludeFile$chr)
+  excludeFile.sort <- bedr.sort.region(excludeFile, check.chr = FALSE,  method = 'lexicographical')
+  
+  bedFileregionsExcluded <- bedr.subtract.region(bedFile.sort, excludeFile.sort, remove.whole.feature = FALSE, check.chr = FALSE)
+  return(data.frame(bedFileregionsExcluded, row.names = NULL))
+}
+
+
+# writeBed
+#'
+#' Simple function to write out bedfiels - useful for checking after merging/subtracting...
+#' @keywords bed
+#' @export
+
+writeBed <- function(df, outDir=getwd(), name='regions.bed'){
+  # svs <- svs %>% 
+  #   mutate(info = paste(sample, type, feature, feature2, sep = "_")) %>% 
+  #   select(chrom, bp, bp2, info)
+  cat(paste(outDir,name, sep='/'))
+  
+  write.table(df, file = paste(outDir,name, sep='/'), row.names=F, col.names=F, sep=" ", quote = FALSE)
+}
+
+
+
+
+
+
+
+
+
 
 
 
@@ -351,21 +450,6 @@ delsRegionEnrichment <- function(bedFiles='bed', region=TRUE, breakpoints=NA,  g
   
   
 }
-
-
-
-
-
-writeBed <- function(svs, bedFile){
-  svs <- svs %>% 
-    mutate(info = paste(sample, type, feature, feature2, sep = "_")) %>% 
-    select(chrom, bp, bp2, info)
-  
-  write.table(svs, file = bedFile, row.names=F, col.names=F, sep=" ", quote = FALSE)
-}
-
-
-
 
 
 #   install_github("favorov/GenometriCorr")
